9c2ad25a878424ed4d1469c44e93600cdf5664cc
paulnarag <naragpaulandrew@gmail.com>
2026-06-05 21:23:28 +0800

    started working on one thing one by one, starting with getting file names

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import fire
-import os
+from pathlib import Path
 
-def createInWD():
-    if os.path.isfile("index.html"):
-        print("index.html already exists")
-        choice=input("Do you want it to be overwritten with the boilerplate HTML? (y/n): ")
-        if choice=="" or choice =="y":
-            indexFile = open("index.html", "")
+HTMLpath = "index.html"
+CSSpath = "style.css"
+JSpath = "script.js"
+
+paths = ["index.html", "style.css", "script.js"]
+
+def createFiles():
+    for file in paths:
+        file = open(file, "w")
+
+for i in range(len(paths)):
+    print(f"The {paths[i].split(".", 1)[1].upper()} file will be named {paths[i]}")
+    choice = input("Are you ok with this? (y/n): ")
+    if choice == "n":
+        newName = input("Your choice of file name: ")
+        paths[i] = newName
+    #if choice == "y":
+
+print(paths)
+
+createFiles()
 
 
-if __name__ == '__main__':
-    fire.Fire(createInWD)
+#def createInWD():
+
+
+
+#if __name__ == '__main__':
+#    fire.Fire(createInWD)
